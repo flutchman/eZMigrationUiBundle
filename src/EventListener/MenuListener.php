9232ec677aeb0aa73270acfe076bb2c20a2f996b
Alexandre Andrieux <alexandre.andrieux.pro@gmail.com>
2019-04-26 15:31:51 +0200

    Adding Admin ui menu item

---
+++ src/EventListener/MenuListener.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Adding items into Back-Office navigation
+ */
+
+namespace Flutchman\eZMigrationAdminUiBundle\EventListener;
+
+use EzSystems\EzPlatformAdminUi\Menu\Event\ConfigureMenuEvent;
+use EzSystems\EzPlatformAdminUi\Menu\MainMenuBuilder;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class MenuListener
+ * @package Flutchman\eZMigrationAdminUiBundle\EventListener
+ */
+class MenuListener implements EventSubscriberInterface
+{
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            ConfigureMenuEvent::MAIN_MENU => ['onMenuConfigure', 0],
+        ];
+    }
+
+    /**
+     * @param ConfigureMenuEvent $event
+     */
+    public function onMenuConfigure(ConfigureMenuEvent $event)
+    {
+        // Getting current menu configuration
+        $menu = $event->getMenu();
+        $factory = $event->getFactory();
+        // options passed from the context (i.e. Content item in Content View)
+        $options = $event->getOptions();
+
+        // your customizations
+        // In this case: adding subitem into "Admin" menu
+        $menu[MainMenuBuilder::ITEM_ADMIN]->addChild(
+            'migration_list',
+            [
+                'label' => 'adminui.migration.menu.label',
+                'route' => 'flutchman_ez_migration_ui_migration_state',
+                'extras' => [
+                    'icon' => 'publish',
+                    'translation_domain' => 'messages',
+                ],
+            ]
+        );
+    }
+}
